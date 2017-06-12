6321c2d6a749dec27903fb996566704c9750cbbc
梁悦 <604140757@qq.com>
2017-06-12 17:00:55 +0800

    Rejected commit: Default

--- src/main/java/Exercises/ForTest.java
+++ src/main/java/Exercises/ForTest.java
@@ -4,7 +4,7 @@ package Exercises;
  * Created by 梁悦 on
  * 2017/5/3.
  */
-public class Test {
+public class ForTest {
     public static void main(String[] args) {
         int k = 1;
         for (int i = 1; i < 5; i++) {
